1df87bc3646268af6fb7a4bacc5c7022f4cefdd5
Maxou305 <maxime.chazard@le-campus-numerique.fr>
2023-10-10 12:07:15 +0200

    initCases OK, initLabyrinte OK

---
+++ scripts/script.js
@@ -0,0 +1,38 @@
+function getLabyrinthes() {
+    return data
+}
+
+let lab = $("<div></div>").attr("id", "labyrinthe")
+
+function initCases(size, difficulty) {
+    let cases = $("<div></div>").attr("class", "cases")
+    if (c.entrance) {
+        cases.addClass("start")
+    }
+    if (c.exit) {
+        cases.addClass("exit")
+    }
+    if (c.walls[0]) {
+        cases.css("border-top", "solid red 1px")
+    }
+    if (c.walls[1]) {
+        cases.css("border-right", "solid red 1px")
+    }
+    if (c.walls[2]) {
+        cases.css("border-bottom", "solid red 1px")
+    }
+    if (c.walls[3]) {
+        cases.css("border-left", "solid red 1px")
+    }
+    lab.append(cases)
+    $("body").append(lab)
+    lab.css("width", size * 100)
+}
+
+function initLabyrinte(size, difficulty) {
+    for (c of data[size][difficulty]) {
+        initCases(size, difficulty)
+    }
+}
+
+initLabyrinte("8", "ex-0")
